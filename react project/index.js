5d972c48d6b2f4ad89b4882db0c775d16eac7d16
[Tania Lopez] <[taniadag5@gmail.com]>
2022-11-06 22:37:26 -0800

    react-updates

--- react project/index.js
+++ react project/index.js
@@ -1,6 +1,65 @@
 import React from "react"
+import ReactDOM from "react-dom"
+
+
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+function Header() {
+    return (
+
+        <header>
+            <nav className= "nav">
+                <img  src="./react-logo.png" className="logo"/>
+
+                <ul className="nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+
+            </nav>
+    </header>
+    
+    )
+}
+
+
+
+function NewPage() {
+    return (
+<div>
+
+            <Header />
+            <MainContent />
+             <Footer />
+
+</div>)}
+
+function MainContent() {
+    return (
+        <div>
+
+            <h1> Reasons Im excited to learn React</h1>
+   
+            <ol>
+                <li>Can start learning more about React</li>
+                <li>Can start learning more about React</li>
+                <li>Can start learning more about React</li>
+                </ol>
+        </div>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+        "© 20xx Lopez development. All rights reserved.
+        </footer>
+    )}
 
 
 
 
-ReactDom.createRoot(document.getElementById("root")).render(navbar)
+ReactDOM.render(<NewPage />, document.getElementById("root"))
