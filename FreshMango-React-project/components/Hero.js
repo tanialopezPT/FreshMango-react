9b937ec4d8d4b22a2d0987e82218bf2199929d42
Tania Lopez <77525392+tanialopezPT@users.noreply.github.com>
2022-11-19 22:52:11 -0800

    Add files via upload

---
+++ FreshMango-React-project/components/Hero.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Hero() {
+    return (
+        <section className="hero">
+            <img src="../images/board.jpeg" className="hero--photo" />
+            <h1 className="hero--header">Bring something new to the table</h1>
+            <p className="hero--text">Our boxes and boards are the perfect way to celebrate or unwind.</p>
+        </section>
+    )
+}
---
+++ FreshMango-React-project/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="../images/IMG_0365.PNG" className="nav--logo" />
+        </nav>
+    )
+}
